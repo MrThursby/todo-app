07fa2754470bb0c51e5d39661a7ccb127709c379
MrThursby <smykoil@ya.ru>
2021-08-25 16:31:29 +0300

    Deleted an unused file

--- frontend/src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import todosReducer from './reducers/todosReducer'
-
-export default configureStore({
-    reducer: {
-        todos: todosReducer
-    }
-})
